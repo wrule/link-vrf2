faf8f47986e09f3de3e1d253f640918c313af7d5
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-03 12:39:34 +0800

    消息

--- dev/index.ts
+++ dev/index.ts
@@ -1,9 +1,11 @@
 import { X } from '../typechain-types';
-import { deployContract, init, meta } from './utils';
+import { deployContract, init, meta, watchContract } from './utils';
 
 async function main() {
   await meta();
   const x = await deployContract<X>('X');
+  watchContract(x);
+  await x.sendMessage('你好，世界');
 }
 
 async function dev() {
