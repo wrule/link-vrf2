241b7ef494967b1827ed7d1b9db1729905294218
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-03 12:54:51 +0800

    状态

--- contracts/X.sol
+++ contracts/X.sol
@@ -9,6 +9,9 @@ contract X is VRFConsumerBase {
     0x326C977E6efc84E512bB9C30f76E30c160eD06FB
   ) { }
 
+  uint256 fee = 0.1 * 10 ** 18;
+  bytes32 keyHash = 0x0476f9a745b61ea5c0ab224d3a6e4c99f0b02fce4da01143a4f70aa80ae76e8a;
+
   function requestRandom() external returns (bytes32) {
 
   }
