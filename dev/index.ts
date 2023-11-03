0bc6e302ec15024e0b11e829771913b9264baaaf
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-03 16:26:32 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -3,7 +3,7 @@ import { deployContract, getContract, init, meta, watchContract } from './utils'
 
 async function main() {
   await meta();
-  const x = await getContract<X>('X', '0x5662f0e89b61e553d1Def8db5cdDf97731581529');
+  const x = await getContract<X>('X', '0x49Cb3DdD9B9acFF4283625366700efF27c47D01f');
   // const x = await deployContract<X>('X');
   watchContract(x);
 
