a877a1b93e93128256f7c719b6e2881aa3573f47
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-03 13:01:54 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -5,10 +5,16 @@ async function main() {
   await meta();
   const x = await deployContract<X>('X');
   watchContract(x);
-  const tx = await x.sendMessage('你好，世界');
-  console.log(tx.hash);
-  const tr = await tx.wait();
-  console.log(tr?.hash);
+
+  // const tx = await x.requestRandom();
+  // console.log(tx.hash);
+  // const tr = await tx.wait();
+  // console.log(tr?.hash);
+
+  // const tx = await x.sendMessage('你好，世界');
+  // console.log(tx.hash);
+  // const tr = await tx.wait();
+  // console.log(tr?.hash);
 }
 
 async function dev() {
