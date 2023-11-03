8ab078d6dd166dcf80dd70c128d6e691373b8465
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-03 12:54:05 +0800

    提交代码

--- contracts/X.sol
+++ contracts/X.sol
@@ -1,7 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-contract X {
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
+
+contract X is VRFConsumerBase {
+  constructor() VRFConsumerBase(
+    0x2bce784e69d2Ff36c71edcB9F88358dB0DfB55b4,
+    0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+  ) { }
+
+  function requestRandom() external returns (bytes32) {
+
+  }
+
+  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+
+  }
+
   event sendMessageEvent(string message);
   function sendMessage(string memory message) public {
     emit sendMessageEvent(message);
