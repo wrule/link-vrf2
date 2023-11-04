4a2d990dd017a537ac0f428c07b05e1f5502de65
jimao <jimao@jimaodeMBP.lan>
2023-11-05 00:00:39 +0800

    提交

--- dev/index.ts
+++ dev/index.ts
@@ -3,14 +3,14 @@ import { deployContract, getContract, init, meta, watchContract } from './utils'
 
 async function main() {
   await meta();
-  const x = await getContract<X>('X', '0xb6434AD5C33B48962d77eD0e5D248eBd4BCC0c99');
-  // const x = await deployContract<X>('X');
+  // const x = await getContract<X>('X', '0xb6434AD5C33B48962d77eD0e5D248eBd4BCC0c99');
+  const x = await deployContract<X>('X');
   watchContract(x);
 
-  const tx = await x.requestRandom(100000, { gasLimit: 600000 });
-  console.log('hash:', tx.hash);
-  const tr = await tx.wait();
-  console.log('hash:', tr?.hash);
+  // const tx = await x.requestRandom({ gasLimit: 600000 });
+  // console.log('hash:', tx.hash);
+  // const tr = await tx.wait();
+  // console.log('hash:', tr?.hash);
 
   // const tx = await x.sendMessage('你好，世界');
   // console.log(tx.hash);
