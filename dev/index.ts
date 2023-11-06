bcfebf780dd99dddd63667a510171a9080f2e704
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-06 11:04:48 +0800

    提交

--- dev/index.ts
+++ dev/index.ts
@@ -3,8 +3,13 @@ import { deployContract, getContract, init, meta, watchContract } from './utils'
 
 async function main() {
   await meta();
-  // const x = await getContract<X>('X', '0x92ED2444ECbbe92F120dafA0965078D7aeDb18E7');
-  const x = await deployContract<X>('X');
+  const x = await getContract<X>('X', '0xae3308Fa2AA7C6eC84E41716aa70BcAc354D89f8');
+  // const x = await deployContract<X>('X');
+  watchContract(x);
+  const tx = await x.requestRandomWords();
+  console.log(tx.hash);
+  const tr = await tx.wait();
+  console.log(tr?.hash);
 }
 
 async function dev() {
